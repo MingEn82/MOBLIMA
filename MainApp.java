9708faa6a9fffa197d112451a2851696401417b9
seekerpig <laserklet@gmail.com>
2022-11-02 17:00:45 +0800

    mainapp trycatch javadoc

--- MainApp.java
+++ MainApp.java
@@ -5,11 +5,26 @@ import Boundary.MainMenuUI;
 
 // import UI.MainMenuUI;
 
+/**
+ * Main App that is executed when the program first runs.
+ */
 public class MainApp {
+
     public static void main(String[] args) {
 
-        MainMenuUI mainMenu = new MainMenuUI();
-        mainMenu.displayMainMenu();
+        try{
+            MainMenuUI mainMenu = new MainMenuUI();
+            mainMenu.displayMainMenu();
+        }
+        catch (Exception e)
+        {
+            /*
+             * Ensure that if the application has any exception, it will be caught and printed out for traceabilty.
+             */
+            e.getMessage();
+            e.printStackTrace();
+        }
+        
 
     }
 }
